ba40700aff174d6acad35f0015718d3155a08cc6
Charles Francoise <charles.francoise@circleci.com>
2023-08-23 00:21:04 +0200

    dice selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,56 @@ import Die from './Die.jsx';
 
 export default function App() {
   const [game, setGame] = useState([
-    { value: 1, used: true },
-    { value: 1, used: true },
-    { value: 1, used: true },
-    { value: 1, used: true },
-    { value: 1, used: true },
+    { value: 1, state: 'used' },
+    { value: 1, state: 'used' },
+    { value: 1, state: 'used' },
+    { value: 1, state: 'used' },
+    { value: 1, state: 'used' },
   ]);
+
   const onThrow = useCallback(() => {
-    const first = game.every((d) => d.used);
+    const first = game.every(
+      (d) => d.state === 'used' || d.state === 'selected',
+    );
 
     const next = game.map((d) => {
-      if (!first && d.used) {
-        return d;
+      if (!first) {
+        if (d.state === 'used') {
+          return { ...d };
+        }
+        if (d.state === 'selected') {
+          return { ...d, state: 'used' };
+        }
       }
-      return { value: Math.floor(Math.random() * 6) + 1, used: false };
+      return { value: Math.floor(Math.random() * 6) + 1, state: 'thrown' };
     });
     setGame(next);
   }, [game]);
+
+  const onSelectDie = useCallback(
+    (i) => {
+      const d = game[i];
+      let state;
+      if (d.state === 'thrown') {
+        state = 'selected';
+      } else if (d.state === 'selected') {
+        state = 'thrown';
+      } else {
+        state = d.state;
+      }
+      setGame([
+        ...game.slice(0, i),
+        { value: d.value, state },
+        ...game.slice(i + 1),
+      ]);
+    },
+    [game],
+  );
+
   return (
     <>
       {game.map((d, i) => (
-        <Die key={i} die={d} />
+        <Die key={i} die={d} onSelect={() => onSelectDie(i)} />
       ))}
       <button onClick={onThrow}>Throw!</button>
     </>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,6 +1,16 @@
 const faces = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
 
-export default function Die({ die }) {
-  const { value, used } = die;
-  return <h1 style={{ opacity: used ? '50%' : '100%' }}>{faces[value - 1]}</h1>;
+export default function Die({ die, onSelect }) {
+  const { value, state } = die;
+  return (
+    <button
+      style={{
+        opacity: state === 'used' ? '50%' : '100%',
+        border: state === 'selected' ? '1px' : '0',
+      }}
+      onClick={onSelect}
+    >
+      {faces[value - 1]}
+    </button>
+  );
 }
