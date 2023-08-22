3d623f5d4c3242a1494913673d0d353ed0121bd8
Charles Francoise <charles.francoise@circleci.com>
2023-08-23 00:42:12 +0200

    use clsx

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,12 +1,15 @@
+import clsx from 'clsx';
+
 const faces = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅'];
 
 export default function Die({ die, onSelect }) {
   const { value, state } = die;
   return (
     <button
-      className={`${state === 'used' ? 'opacity-50' : 'opacity-100'} ${
-        state === 'selected' ? 'border' : 'border-none'
-      }`}
+      className={clsx([
+        state === 'used' ? 'opacity-50' : 'opacity-100',
+        state === 'selected' ? 'border' : 'border-none',
+      ])}
       onClick={onSelect}
     >
       {faces[value - 1]}
